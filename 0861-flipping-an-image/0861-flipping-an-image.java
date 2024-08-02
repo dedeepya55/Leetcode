90276cd58da4cfca97d3d5dc2da25bfc58e081b1
dedeepya55 <119920060+dedeepya55@users.noreply.github.com>
2024-08-02 21:45:34 +0530

    Time: 1 ms (47.5%), Space: 44.6 MB (35.84%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<(image.length+1)/2;j++)
+            {
+                int temp=image[i][j];
+                image[i][j]=image[i][image.length-j-1];
+                image[i][image.length-j-1]=temp;
+            }
+            for(int j=0;j<image.length;j++)
+            {
+                image[i][j]=image[i][j]^1;
+            }
+        }
+        return image;
+    }
+}
