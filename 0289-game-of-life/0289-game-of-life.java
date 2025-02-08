b48453fe329418d1badba27441cdf5f9d81c9b7d
DedeepyaKalavakuri <119920060+dedeepya55@users.noreply.github.com>
2025-02-08 22:41:17 +0530

    Time: 3 ms (4.94%), Space: 41.6 MB (71.49%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int r=board.length;
+        int c=board[0].length;
+       for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            int live=-board[i][j];
+            for(int i1=i-1;i1<=i+1;i1++){
+                for(int j1=j-1;j1<=j+1;j1++){
+                    if(i1>=0 && i1<r && j1>=0 && j1<c && board[i1][j1]>0){
+                        // System.out.println(live);
+                        live++;
+                    }
+                }
+            }
+            if(board[i][j]==1 && (live>3 || live<2)){
+                board[i][j]=2; //dead
+            }
+            if(board[i][j]==0 && (live==3)){
+                System.out.println(board[i][j]);
+                board[i][j]=-1; //live
+            }
+        }
+       }
+       for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(board[i][j]==2){
+                board[i][j]=0;
+            }
+            else if(board[i][j]==-1){
+                System.out.println(board[i][j]);
+                board[i][j]=1;
+            }
+        }
+       }
+    }
+}
