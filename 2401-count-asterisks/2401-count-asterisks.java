e5056807b8e7d692cfdfa4d41e9e1e7e05bfafa6
DedeepyaKalavakuri <119920060+dedeepya55@users.noreply.github.com>
2025-02-25 22:38:05 +0530

    Time: 1 ms (100%), Space: 41.8 MB (28.17%) - LeetHub

---
+++ 2401-count-asterisks/2401-count-asterisks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        boolean insideBars = false;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '|') {
+                insideBars = !insideBars; 
+            } else if (ch == '*' && !insideBars) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
