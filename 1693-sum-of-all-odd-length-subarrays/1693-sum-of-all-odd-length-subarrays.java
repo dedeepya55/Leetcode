f16149e4079822bf4dae4293fa70beaa1581ef26
DedeepyaKalavakuri <119920060+dedeepya55@users.noreply.github.com>
2025-06-09 18:54:27 +0530

    Time: 0 ms (100%), Space: 41.1 MB (88.46%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,19 +1,12 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-    int ans=0;
+        int sum=0;
 
-    int prevEvenSum = 0;
-    int prevOddSum = 0; 
-
-    for (int i = 0; i < arr.length; ++i) {
-      final int currEvenSum = prevOddSum + ((i + 1) / 2) * arr[i];
-      final int currOddSum = prevEvenSum + (i / 2 + 1) * arr[i];
-      System.out.println(currEvenSum+" "+currOddSum);
-      ans += currOddSum;
-      prevEvenSum = currEvenSum;
-      prevOddSum = currOddSum;
-    }
-
-    return ans;
+        for(int i=0;i<arr.length;i++){
+            int contribution=(i+1)*(arr.length-i);
+            int oC=(contribution+1)/2;
+            sum+=oC*arr[i];
+        }
+        return sum;
   }
 }
